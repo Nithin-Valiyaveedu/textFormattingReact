6f2be6bc06c85c0c345d89dc48ac227d03b5f80a
Nithin-Valiyaveedu <nithinvaliya@gmail.com>
2022-02-07 11:26:21 +0530

    react trxt editor poc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from 'draftjs-to-html';
+import { Editor as Editorrr, EditorState, convertFromRaw, convertToRaw } from "draft-js";
+// import storedState from "./storedState.json";
+// console.log(storedState);
+const App = () => {
 
-function App() {
+  const [editorState, setEditorState] = useState(() =>
+  EditorState.createEmpty()
+);
+
+  const [styledState, setStyledState] = useState(convertToRaw(editorState.getCurrentContent()))
+
+
+
+  const contentState = convertFromRaw(styledState);
+  const state = EditorState.createWithContent(contentState);
+
+  useEffect(() => {
+    // console.log(editorState.getCurrentContent());
+    // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
+    // console.log(editorState.getCurrentContent().getPlainText());
+    console.log((convertToRaw(editorState.getCurrentContent())));
+    setStyledState(convertToRaw(editorState.getCurrentContent()))
+
+
+
+  }, [editorState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>React Editors</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div style={{ border: "1px solid black", padding: '2px', minHeight: '400px' }}>
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+        />
+      </div>
+
     </div>
+    <div className="App">
+      <Editorrr editorState={state} readOnly={true} />
+    </div>
+    </>
   );
-}
+};
 
 export default App;
